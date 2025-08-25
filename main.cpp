255cac7628956e5412a3c5d54fb31f4c16fb4f34
TMCsgs <gihub@stegasoft.de>
2025-08-25 17:31:22 +0200

    changed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_video.h"
 #include <SDL3/SDL.h>
 
+
 int main() {
-    
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("game", 800, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 }
+
